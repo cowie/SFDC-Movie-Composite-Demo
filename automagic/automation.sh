a35163a389233af7bb2c082b049c336ae6850c1a
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-01-30 12:25:13 -0800

    Automation built, time to test this out

--- automagic/automation.sh
+++ automagic/automation.sh
@@ -1,5 +1,8 @@
 #batcher
-sfdx force:org:create -f config/project-scratch-def.json -s -a movieDevOrg -d 7
+echo Welcome to the automagical thing. Gonna make a scratch, make it good, then make a heroku. Kick back.
+echo Please enter a name for the scratch org
+read orgName
+sfdx force:org:create -f config/mcdOrgDefinition.json -s -a $orgName -d 7
 sfdx force:source:push
 sfdx force:user:permset:assign -n MovieAppPermSet
 sfdx force:data:bulk:upsert -s Production_Country__c -f data/prepared/Production_Country__c.csv -i iso_3166_1__c -w 1
